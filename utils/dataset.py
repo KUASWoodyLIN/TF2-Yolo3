d8a509d23ccd4501c68a9d806340d18077c573ad
KUASWoodyLin <jj7158761@gmail.com>
2019-05-10 20:33:06 +0800

    working on data input

--- utils/dataset.py
+++ utils/dataset.py
@@ -4,13 +4,96 @@ import tensorflow_datasets as tfds
 train_data, info = tfds.load("coco2014", split=tfds.Split.TRAIN, with_info=True)
 valid_data = tfds.load("coco2014", split=tfds.Split.VALIDATION)
 test_data = tfds.load("coco2014", split=tfds.Split.TEST)
+labels = info.features['labels'].names
 
-def flip(x):
+
+# FeaturesDict({
+#     'image': Image(shape=(None, None, 3), dtype=tf.uint8),
+#     'image/filename': Text(shape=(), dtype=tf.string, encoder=None),
+#     'objects': SequenceDict({
+#         'bbox': BBoxFeature(shape=(4,), dtype=tf.float32),
+#         'is_crowd': Tensor(shape=(), dtype=tf.bool),
+#         'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=80),
+#     }),
+# })
+
+# FeaturesDict({
+#     'image': Image(shape=(None, None, 3), dtype=tf.uint8),
+#     'image/filename': Text(shape=(), dtype=tf.string, encoder=None),
+#     'labels': Sequence(shape=(None,), dtype=tf.int64, feature=ClassLabel(shape=(), dtype=tf.int64, num_classes=20)),
+#     'labels_no_difficult': Sequence(shape=(None,), dtype=tf.int64, feature=ClassLabel(shape=(), dtype=tf.int64, num_classes=20)),
+#     'objects': SequenceDict({
+#         'bbox': BBoxFeature(shape=(4,), dtype=tf.float32),
+#         'is_difficult': Tensor(shape=(), dtype=tf.bool),
+#         'is_truncated': Tensor(shape=(), dtype=tf.bool),
+#         'label': ClassLabel(shape=(), dtype=tf.int64, num_classes=20),
+#         'pose': ClassLabel(shape=(), dtype=tf.int64, num_classes=5),
+#     }),
+# })
+
+
+
+def parse_aug_fn(dataset):
+    """
+    Image Augmentation function
+    """
+    # (None, None, 3)
+    x = tf.cast(dataset['image'], tf.float32) / 255.
+    # (x1, y1, x2, y2, class)
+    y = dataset['objects']['bbox']
+    x, y = flip(x, y)
+
+    # 觸發顏色轉換機率50%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.5, lambda: color(x), lambda: x)
+    # 觸發影像旋轉機率0.25%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.75, lambda: rotate(x), lambda: x)
+    # 觸發影像縮放機率50%
+    x = tf.cond(tf.random.uniform([], 0, 1) > 0.5, lambda: zoom(x), lambda: x)
+    # 將輸出標籤轉乘One-hot編碼
+    return x, y
+
+
+def parse_fn(dataset):
+    x = tf.cast(dataset['image'], tf.float32) / 255.  # 影像標準化
+    # 將輸出標籤轉乘One-hot編碼
+    y = tf.one_hot(dataset['label'], 10)
+    return x, y
+
+
+def transform_targets(y_train, anchors, anchor_masks, classes):
+    y_outs = []
+    grid_size = 13
+
+    # calculate anchor index for true boxes
+    anchors = tf.cast(anchors, tf.float32)
+    anchor_area = anchors[..., 0] * anchors[..., 1]
+    box_wh = y_train[..., 2:4] - y_train[..., 0:2]
+    box_wh = tf.tile(tf.expand_dims(box_wh, -2),
+                     (1, 1, tf.shape(anchors)[0], 1))
+    box_area = box_wh[..., 0] * box_wh[..., 1]
+    intersection = tf.minimum(box_wh[..., 0], anchors[..., 0]) * \
+        tf.minimum(box_wh[..., 1], anchors[..., 1])
+    iou = intersection / (box_area + anchor_area - intersection)
+    anchor_idx = tf.cast(tf.argmax(iou, axis=-1), tf.float32)
+    anchor_idx = tf.expand_dims(anchor_idx, axis=-1)
+
+    y_train = tf.concat([y_train, anchor_idx], axis=-1)
+
+    for anchor_idxs in anchor_masks:
+        y_outs.append(transform_targets_for_output(
+            y_train, grid_size, anchor_idxs, classes))
+        grid_size *= 2
+
+    return tuple(y_outs)
+
+
+def flip(x, y):
     """
     flip image(翻轉影像)
     """
     x = tf.image.random_flip_left_right(x)  # 隨機左右翻轉影像
-    return x
+    y[..., 2::-2] = 1 - y[..., 0::2]
+    return x, y
 
 def color(x):
     """
@@ -40,6 +123,9 @@ def zoom(x, scale_min=0.6, scale_max=1.4):
     sw = w * scale  # 縮放後影像寬度
     x = tf.image.resize(x, (sh, sw))  # 影像縮放
     x = tf.image.resize_image_with_crop_or_pad(x, h, w)  # 影像裁減和填補
+
+    box[:, [0, 2]] = box[:, [0, 2]] * scale + dx
+    box[:, [1, 3]] = box[:, [1, 3]] * scale + dy
     return x
 
 
